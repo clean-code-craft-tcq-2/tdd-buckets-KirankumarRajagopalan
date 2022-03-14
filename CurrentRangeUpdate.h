5f0fd8bd41e771a0a80763b088b1a47c8fafd630
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-03-14 20:36:31 +0530

    Create CurrentRangeUpdate.h

---
+++ CurrentRangeUpdate.h
@@ -0,0 +1,5 @@
+extern int CurrentRangesWithCount[][3];
+
+int CurrentRangeUpdate (int* CurrentValues, int samples);
+int* Sort_CurrentValues(int* CurrentValues, int samples);
+void PrintonConsole(int CurrentRange_Count);
