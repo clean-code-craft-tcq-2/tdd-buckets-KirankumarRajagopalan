0e4a05b4609827ccc00d1e78079f68475c4e1381
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-03-25 14:36:11 +0530

    Update CurrentRangeChecker.cpp

--- CurrentRangeChecker.cpp
+++ CurrentRangeChecker.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+#include "catch.hpp"
 #include "CurrentRangeUpdate.h"
 
 TEST_CASE("Checks if number of ranges is updated with single continuous range of current values") {
